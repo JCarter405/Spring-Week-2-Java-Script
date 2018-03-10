10f37127064cdc5b031bf6bbbd6976a8648443e2
Justin Carter <caufield365@yahoo.com>
2018-03-10 12:36:07 -0600

    new adds

---
+++ functions_1.js
@@ -0,0 +1,16 @@
+
+var printVerse = function (num){
+
+	var name = "bottles";
+	if(num === 1) { name = "bottle"; }
+
+	console.log(num + " " + name + " bottles of beer on the wall");
+	console.log(num + " " + name + " of beer");
+	console.log("You take one down, you pass it around.");
+	num--;
+	console.log(num + " bottles of beer on the wall\n");
+}
+
+for(var x = 99; x > 0; x--){
+	printVerse(x)
+}
