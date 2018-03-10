8eacce363aab5d3e54d61fbf69efd5097cdde7d9
Justin Carter <caufield365@yahoo.com>
2018-03-10 10:11:45 -0600

    additional function

--- index.js
+++ index.js
@@ -2,23 +2,22 @@
 
 var beers = 99;
 
+function printVerse(num){
+
+	var name = "bottles";
+	if(num === 1) { name = "bottle"; }
+
+	console.log(num + " " + name + " bottles of beer on the wall");
+	console.log(num + " " + name + " of beer");
+	console.log("You take one down, you pass it around.");
+	num--;
+	console.log(num + " bottles of beer on the wall\n");
+	return num;
+}
+
 // repeat the section 99 times
 
 while(beers > 0){
-
-	if(beers === 1){
-		console.log(beers + " bottles of beer on the wall");
-		console.log(beers + " bottles of beer");
-		console.log("You take one down, you pass it around.");
-		beers--;
-		console.log(beers + " bottles of beer on the wall\n"); 
-			// beers = beers - 1; // does the same thing as beers --;
-			// beers = beers + 1; // does the same thing as beers ++;
-} else {
-	console.log(beers + " bottles of beer on the wall");
-	console.log(beers + " bottles of beer");
-	console.log("You take one down, you pass it around.");
-	beers--;
-	console.log(beers + " bottles of beer on the wall"); 
-	}
+	// console.log ("No more bottles of bee on the wall!! Go buy more beer.\n");
+	beers = printVerse(beers);
 }
